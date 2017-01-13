c71a03576701e41042e349d5181dc68b111890ff
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-12 20:29:26 -0800

    fix: reduce function length to 25 lines

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,11 +6,23 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 17:12:36 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/12 19:47:36 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/12 20:23:53 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_put_char(char c);
+
+void	ft_display_comb(char i, char j, char k)
+{
+	if (j != k && i != j)
+	{
+		ft_put_char(i);
+		ft_put_char(j);
+		ft_put_char(k);
+		ft_put_char(',');
+		ft_put_char(' ');
+	}
+}
 
 void	ft_print_comb(void)
 {
@@ -27,14 +39,7 @@ void	ft_print_comb(void)
 		{
 			while (k <= '9')
 			{
-				if (j != k && i != j)
-				{
-					ft_put_char(i);
-					ft_put_char(j);
-					ft_put_char(k);
-					ft_put_char(',');
-					ft_put_char(' ');
-				}
+				ft_display_comb(i, j, k);
 				k++;
 			}
 			k = j + 1;
