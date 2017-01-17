75ea223997a454c380bf3737171feb4186f45042
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-17 11:30:20 -0800

    Exercise 04 Done

--- ex04/ft_strncpy.c
+++ ex04/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 10:11:41 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/17 10:29:58 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/17 11:23:24 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@ char	*ft_strncpy(char *dst, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (i < n && src[i])
+	while (i < n && (src[i] && dst[i]))
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	while (i < n)
+	while (dst[i])
 		dst[i++] = '\0';
 	return (dst);
 }
