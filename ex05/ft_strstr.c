a11291e3ce5bc85e9dcb1229d48a8ed77e2c83cc
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-17 14:20:38 -0800

    Exercise 05 done

---
+++ ex05/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/17 13:15:32 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/01/17 14:19:40 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int j;
+	int size;
+
+	i = 0;
+	j = 0;
+	size = 0;
+	while (to_find[size])
+		size++;
+	if (size == 0)
+		return (str);
+	while (str[i])
+	{
+		while (to_find[j] == str[i + j])
+		{
+			if (j == size - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
