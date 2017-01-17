e7f9ebe805297f7a613f63fe193458500c85bd1e
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-16 22:27:32 -0800

    norm fixes

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,9 +6,11 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 11:14:57 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/16 12:31:02 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/16 15:53:34 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int		ft_putchar(char c);
 
@@ -17,9 +19,15 @@ void	ft_putnbr(int nb)
 	if (nb < 0)
 	{
 		ft_putchar('-');
-		nb = -nb;
+		if (nb == -2147483648)
+		{
+			ft_putnbr(-(nb / 10));
+			ft_putnbr(-(nb % 10));
+		}
+		else
+			ft_putnbr(-nb);
 	}
-	if (nb >= 10)
+	else if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
