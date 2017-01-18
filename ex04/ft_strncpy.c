5122ec5c187ba4830b5052b34e475655b2b7bdcc
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-18 12:38:04 -0800

    fixed strncpy

--- ex04/ft_strncpy.c
+++ ex04/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 10:11:41 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/17 21:13:57 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/18 12:34:22 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ char	*ft_strncpy(char *dst, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (i < n && (src[i] && dst[i]))
+	while (i < n && src[i])
 	{
 		dst[i] = src[i];
 		i++;
