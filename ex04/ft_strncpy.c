afce3a842e97875ff0025e6bf5bb70b35b8e913d
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-17 21:15:13 -0800

    fix when source len is less than n

--- ex04/ft_strncpy.c
+++ ex04/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 10:11:41 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/17 11:23:24 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/17 21:13:57 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strncpy(char *dst, char *src, unsigned int n)
 		dst[i] = src[i];
 		i++;
 	}
-	while (dst[i])
+	while (i < n && dst[i])
 		dst[i++] = '\0';
 	return (dst);
 }
