56f5fb1b19c36953ef2e039f0fd20532ad611486
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-21 23:11:09 -0800

    fixed conditions priority

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -12,6 +12,12 @@
 
 int	nmatch(char *s1, char *s2)
 {
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+			return (nmatch(s1, ++s2));
+		return (nmatch(s1, s2 + 1) || nmatch(s1 + 1, s2));
+	}
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 != *s1)
@@ -20,11 +26,5 @@ int	nmatch(char *s1, char *s2)
 		return (0);
 	if (*s1 == *s2)
 		return (nmatch(++s1, ++s2));
-	if (*s2 == '*')
-	{
-		if (*s1 == '\0')
-			return (nmatch(s1, ++s2));
-		return (nmatch(s1 + 1, s2) + nmatch(s1, s2 + 1));
-	}
 	return (-1);
 }
