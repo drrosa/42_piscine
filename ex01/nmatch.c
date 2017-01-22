8a2a33e0a0ed6cfb09a2d4ecd6b5aa22bf60db85
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-21 22:51:42 -0800

    fixed unsequenced modification error

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -24,7 +24,7 @@ int	nmatch(char *s1, char *s2)
 	{
 		if (*s1 == '\0')
 			return (nmatch(s1, ++s2));
-		return (nmatch(++s1, s2) + nmatch(s1, ++s2));
+		return (nmatch(s1 + 1, s2) + nmatch(s1, s2 + 1));
 	}
 	return (-1);
 }
