ec6e64a6bb800d0ced2ab8f6a392ea210985d261
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-23 17:38:27 -0800

    Ex05 Done.

---
+++ ex05/ft_is_sort.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/23 17:35:19 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/01/23 17:37:45 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+	int i;
+
+	i = 0;
+	while(i < length - 1)
+	{
+		if (f(tab[i], t[i + 1]) <= 0)
+			i++;
+		else
+			break;
+	}
+	if (i == length - 1)
+		return (1);
+	i = 0;
+	while (i < length - 1)
+	{
+		if (f(tab[i], t[i + 1]) >= 0)
+			i++;
+		else
+			break;
+	}
+	if (i == length - 1)
+		return (1);
+	return (0);
+}
