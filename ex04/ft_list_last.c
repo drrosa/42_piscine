a691e93515e5871e9b9d98075453e3b374a18ca6
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-25 17:15:38 -0800

    fixed empty list

--- ex04/ft_list_last.c
+++ ex04/ft_list_last.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 20:21:56 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/25 17:08:40 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/25 17:14:37 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@ t_list	*ft_list_last(t_list *begin_list)
 	t_list *this_node;
 
 	this_node = begin_list;
-	while (this_node->next)
+	if (begin_list)
 	{
-		this_node = this_node->next;
+		while (this_node->next)
+		{
+			this_node = this_node->next;
+		}
+		return (this_node);
 	}
-	return (this_node);
+	else
+		return (this_node);
 }
