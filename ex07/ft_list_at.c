5c6419dcc51572b902d87a07511d6a3275443f71
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-25 20:38:48 -0800

    Ex07

--- ex07/ft_list_at.c
+++ ex07/ft_list_at.c
@@ -6,11 +6,12 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 20:21:10 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/25 20:28:11 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/25 20:36:11 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
 t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
 {
