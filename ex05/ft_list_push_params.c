e7e51ad19e294eba8b9bec5585029a7116dc81a6
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-01-25 19:47:19 -0800

    fixes

--- ex05/ft_list_push_params.c
+++ ex05/ft_list_push_params.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 21:31:49 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/01/25 16:24:22 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/01/25 19:46:41 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ t_list	*ft_list_push_params(int ac, char **av)
 	t_list	*temp_node;
 	int		i;
 
-	list = NULL;
-	i = 1;
+	if (ac == 1)
+		return (0);
+	list = ft_create_elem(av[1]);
+	i = 2;
 	while (i < ac)
 	{
-		temp_node = ft_create_elem(av[i]);
-		list = temp_node;
-		temp_node->next = list;
+		temp_node = list;
+		list = ft_create_elem(av[i]);
+		list->next = temp_node;
 		i++;
 	}
 	return (list);
