c892870cc8df3994627e1f3dfeb9f0ef50a41d6c
David Rosa Tamsen <drosa-ta@student.42.us.org>
2017-02-01 16:42:55 -0800

    changed prototype

--- libbsq.h
+++ libbsq.h
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 01:18:34 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/01/31 21:59:33 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/02/01 16:38:07 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_put_int_arr(int **arr, int length, int height);
 int     **ft_ch_to_int(char **ch_arr, int height, int length, char c);
 int		ft_length(char *str);
 int		ft_height(char *str);
-int		*ft_algorithm(int **arr, int height, int length);
+int		*ft_algorithm(int **arr, int height, int length, int *max);
 void    ft_draw_square(char **d_arr, int *limit, int size, char c);
 int     ft_key_len(char *str);
 
