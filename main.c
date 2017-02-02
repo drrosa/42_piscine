0b3f57a4988d18b327bf6ecd29eb1792b10a3d62
gguiulfo <giacomoguiulfo@gmail.com>
2017-02-01 23:24:55 -0800

    fixed map full of obstacles

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 00:19:44 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/02/01 22:40:22 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/02/01 23:24:16 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,24 @@
 void	ft_find_bsq(char *key, char **d_arr, int *x_y)
 {
 	int		**int_arr;
+	int		length;
+	int		height;
 
-	if (x_y[1] == 1 && x_y[0] == 1)
+	length = x_y[0];
+	height = x_y[1];
+	x_y[0] = -1;
+	x_y[1] = -1;
+	if (length == 1 && height == 1)
 	{
 		if (d_arr[0][0] == key[0])
-		{
 			ft_putchar(key[2]);
-			ft_putchar('\n');
-		}
 		else
-		{
 			ft_putchar(key[1]);
-			ft_putchar('\n');
-		}
+		ft_putchar('\n');
 		return ;
 	}
-	int_arr = ft_ch_to_int(d_arr, x_y[1], x_y[0], key[0]);
-	x_y = ft_algorithm(int_arr, x_y[1], x_y[0], x_y);
+	int_arr = ft_ch_to_int(d_arr, height, length, key[0]);
+	x_y = ft_algorithm(int_arr, height, length, x_y);
 	if (x_y[0] != -1 && x_y[1] != -1)
 		ft_draw_square(d_arr, x_y, int_arr[x_y[0]][x_y[1]] - 1, key[2]);
 	ft_put_ch_arr(d_arr);
@@ -46,17 +47,13 @@ void	ft_handle_bsq(char *map)
 	int		*x_y;
 
 	x_y = (int*)malloc(sizeof(int) * 2);
-	x_y[0] = -1;
-	x_y[1] = -1;
 	x_y[0] = ft_length(map);
 	x_y[1] = ft_height(map);
 	key = ft_get_key(map);
 	d_arr = ft_get_arr(map);
 	if (!ft_valid_key(map) || !ft_validate_map(d_arr, key, x_y[0]) ||
 			x_y[1] <= 0 || ft_key_len(map) != x_y[1] || ft_key_len(map) <= 0)
-	{
 		ft_puterr("map error\n");
-	}
 	else
 		ft_find_bsq(key, d_arr, x_y);
 }
