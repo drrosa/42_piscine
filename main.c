1e84ca39bb61f9324a3bedc403f4bef0d90864b1
gguiulfo <giacomoguiulfo@gmail.com>
2017-02-01 22:17:37 -0800

    working dirty version

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gguiulfo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 00:19:44 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/02/01 21:50:41 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/02/01 22:16:52 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 #include "libbsq.h"
 #include <stdlib.h>
 
+void find_bsq()
+{
+
+}
+
 int		main(int argc, char **argv)
 {
 	char	*map;
 	char	**d_arr;
 	int		**int_arr;
-	int		length;
-	int		height;
 	int		*x_y;
 	int		i;
 	char	*key;
@@ -45,19 +48,19 @@ int		main(int argc, char **argv)
 			ft_puterr("map error\n");
 		else
 		{
-			length = ft_length(map);
-			height = ft_height(map);
+			x_y[0] = ft_length(map);
+			x_y[1] = ft_height(map);
 			key = ft_get_key(map);
 			d_arr = ft_get_arr(map);
-		if (!ft_valid_key(map) || !ft_validate_map(d_arr, key, length) || height <= 0 ||
-			ft_key_len(map) != height || ft_key_len(map) <= 0)
+		if (!ft_valid_key(map) || !ft_validate_map(d_arr, key, x_y[0]) || x_y[1] <= 0 ||
+			ft_key_len(map) != x_y[1] || ft_key_len(map) <= 0)
 		{
 			ft_puterr("map error\n");
-			return (0);
 		}
-		if (height == 1 && length == 1)
+		else{
+		if (x_y[1] == 1 && x_y[0] == 1)
 		{
-			if(d_arr[0][0] == key[0])
+			if (d_arr[0][0] == key[0])
 			{
 				ft_putchar(key[2]);
 				ft_putchar('\n');
@@ -69,11 +72,12 @@ int		main(int argc, char **argv)
 			}
 			return (0);
 		}
-		int_arr = ft_ch_to_int(d_arr, height, length, key[0]);
-		x_y = ft_algorithm(int_arr, height, length, x_y);
-		if(x_y[0] != -1 && x_y[1] != -1)
+		int_arr = ft_ch_to_int(d_arr, x_y[1], x_y[0], key[0]);
+		x_y = ft_algorithm(int_arr, x_y[1], x_y[0], x_y);
+		if (x_y[0] != -1 && x_y[1] != -1)
 			ft_draw_square(d_arr, x_y, int_arr[x_y[0]][x_y[1]] - 1, key[2]);
 		ft_put_ch_arr(d_arr);
+		}
 		}
 	}
 	return (0);
